88e6d26bd080b7d8fe85142a864a24e057fbd376
Phong <phong.luu@saleshood.com>
2018-04-25 22:57:26 +0700

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_class?
+
+  def current_class?(test_path)
+    return 'active' if request.path == test_path
+    ''
+  end
 end
