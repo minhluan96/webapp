6bab548dc00cc3edeba9e1558cb965f62ed88c11
Phong <phong.luu@saleshood.com>
2018-05-10 16:23:20 +0700

    test webhook, will revert later

--- app/controllers/sharepoint_webhook_controller.rb
+++ app/controllers/sharepoint_webhook_controller.rb
@@ -1,6 +1,7 @@
 class SharepointWebhookController < ApplicationController
   def initialize_webhook
     token = params[:validationtoken]
+    p "TOKEN #{token}"
     RestClient.post("https://saleshoodinc.sharepoint.com/sites/dientesting/_api/web/lists(guid'212e125c-f303-4c4d-9c96-5189de72ba41')/subscriptions", token, {"Content-Type" => "text/plain"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources 'sharepoint_webhook' do
     collection do
-      get 'initialize_webhook'
+      post 'initialize_webhook'
     end
   end
 
