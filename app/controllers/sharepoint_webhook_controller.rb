ffaaaee7e268b46698322a04f218c28c7d8e1052
Phong <phong.luu@saleshood.com>
2018-05-11 15:46:45 +0700

    test webhook, will revert later

--- app/controllers/sharepoint_webhook_controller.rb
+++ app/controllers/sharepoint_webhook_controller.rb
@@ -2,8 +2,8 @@ class SharepointWebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def initialize_webhook
-    @token = params[:validationtoken]
-    puts "PARAMS = #{params[:value]}"
+    # @token = params[:validationtoken]
+    puts "PARAMS = #{params}"
     render :plain => @token
   end
 end
