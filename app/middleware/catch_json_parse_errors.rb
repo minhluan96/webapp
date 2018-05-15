ccf37a0141ff3b045ad4243170bcb669f99223cf
Phong <phong.luu@saleshood.com>
2018-05-15 16:50:20 +0700

    test webhook, will revert later

--- app/middleware/catch_json_parse_errors.rb
+++ app/middleware/catch_json_parse_errors.rb
@@ -7,16 +7,13 @@ class CatchJsonParseErrors
     begin
       @app.call(env)
     rescue ActionDispatch::Http::Parameters::ParseError => error
-      if env['HTTP_ACCEPT'] =~ /application\/json/
-        error_output = "There was a problem in the JSON you submitted: #{error}"
-        p "error_output= #{error_output}"
-        return [
-            400, { "Content-Type" => "application/json" },
-            [ { status: 400, error: error_output }.to_json ]
-        ]
-      else
-        raise error
-      end
+      error_output = "There was a problem in the JSON you submitted: #{error}"
+      p env['HTTP_ACCEPT']
+      p "error_output= #{error_output}"
+      return [
+          400, { "Content-Type" => "application/json" },
+          [ { status: 400, error: error_output }.to_json ]
+      ]
     end
   end
 end
