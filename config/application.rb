9b60c20f8f7af4f074af23722b22835711fd5aff
Phong <phong.luu@saleshood.com>
2018-05-15 16:35:48 +0700

    test webhook, will revert later

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Webapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.middleware.insert_before ActionDispatch::ParamsParser, "CatchJsonParseErrors"
+    require "./app/middleware/catch_json_parse_errors.rb"
+    config.middleware.insert_before Rack::Head, CatchJsonParseErrors
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
