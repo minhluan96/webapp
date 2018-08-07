4c5a07090abfa5881629e4c46b41070d49c41111
Phong <phong.luu@saleshood.com>
2018-08-07 12:59:20 +0700

    .

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -15,8 +15,10 @@ class Admin::OrdersController < ApplicationController
     @order_detail.cogs = @case.capital
     @order_detail.quantity = 1
     @order_detail.created_at = params[:created_at]
-    @order_detail.case_category = CaseCategory.where(case_id: @case.id, category_id: params[:category]).first
+    @order_detail.case_category = CaseCategory.where(case_id: @case.id, category_id: params[:category_id]).first
     @order_detail.revenue = @order_detail.price - @order_detail.cogs
     @order_detail.save!
+    @order_details = OrderDetail.from_this_month.group_by_day(:created_at)
+    render json: [{name: 'Doanh Thu', data: @order_details.sum(:price)}, {name: 'Lợi Nhuận', data: @order_details.sum(:revenue)}].to_json
   end
 end
